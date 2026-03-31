a08e0736a0f1fdc2b4cef6c8922c04474bcc2655
MeiyanChen25 <cynciachen@CynciadeMacBook-Pro-5.local>
2026-03-31 16:17:23 -0400

    add AIEthics.ts

---
+++ frontend/src/types/AIEthics.ts
@@ -0,0 +1,12 @@
+export type AIEthics = {
+    UniqueID: string;
+    Tag: string;
+    RelatedInfo: string;
+    MinPrice: BigInteger;
+    MaxPrice: BigInteger;
+    ConsiderationCount: BigInteger;
+    ConsiderationInfo: string;
+    ConsiderationScore: BigInteger;
+    TotalScore: BigInteger;
+    SimilarTools: string;
+}
