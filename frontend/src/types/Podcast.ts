97c84fa02a2fdf9627967e47827ac0c88e9662b3
Brendan <brendy1628@gmail.com>
2026-04-07 16:00:10 -0400

    Add Podcast types file

--- frontend/src/types/Podcast.ts
+++ frontend/src/types/Podcast.ts
@@ -1,19 +1,10 @@
-export type Episode = {
-  number: number;
-  date: string;
-  url: string;
-};
-
-export type EpisodeFilters = {
-  minEp: number | "";
-  maxEp: number | "";
-  startDate: string;
-  endDate: string;
-};
-
 export type PodcastInfo = {
+  podcast_number: number;
   podcast_id: number;
   title: string;
   description: string;
+  podcast_url: string;
   image_url: string;
+  date: number;
+  speaker: string;
 };
